94a56fa1dccf64795b31c9699f031ac9afdbbaa3
ming <2501531415@qq.com>
2022-08-29 16:29:03 +0800

    新增echart组件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,104 +1,228 @@
 <template>
-	<view class="content1">
-		<!-- <my-upload @beforeRead="beforeRead" @success="success" @fail="fail" :file-list="fileList"
-		 url="http://192.168.0.149:3000/upload" :width="100" :count="5" accept="image" @click-delete="clickDelete" @click-preview="preView"/> -->
-		 <!-- <my-side-bar :side-bar-list="categoryData" @change="onChange" :active-index="3" @badge-click="badgeClick" active-border-color="green" badgeBgC="green"/> -->
-		 <!-- <my-refresh style="flex:1">
-				<view class="right">
-					<view class="n" v-for="(item,index) in 999" :key="index">
-						{{item}}
-					</view>
-				</view>
-		</my-refresh> -->
-		<!-- <my-nav-bar @left-click="leftClick" border left-text="返回" left-allow>
-			<view class="b" slot="center">
-				中间
-			</view>
-			<view class="b" slot="right">
-				右边
-			</view>
-		</my-nav-bar> -->
-		<!-- <my-tab-bar :tabBar="tabBar" @change="change"/> -->
-		<my-drop-down :options="options"/>
-	</view>
+  <view>
+    <uni-ec-canvas
+      class="uni-ec-canvas"
+      id="line-chart"
+      canvas-id="multi-charts-line"
+      :ec="ec"
+    ></uni-ec-canvas>
+
+    <uni-ec-canvas
+      class="uni-ec-canvas"
+      id="pie-chart"
+      canvas-id="multi-charts-pie"
+      :ec="ec2"
+    ></uni-ec-canvas>
+  </view>
 </template>
 
 <script>
-	import tabBarMixins from '@/config/tabBar'
-	export default {
-		mixins:[tabBarMixins],
-		data() {
-			return {
-				title: 'Hello',
-				swiperData:[
-					 'http://192.168.0.149:3000/public/img/55.jpg',
-					 'http://192.168.0.149:3000/public/img/10.jpg',
-					 'http://192.168.0.149:3000/public/img/11.jpg'
-				],
-				navData:['最新','福利','你好'],
-				listData:30,
-				pageNo:1,
-				currentPage:1,
-				fileList:[],
-				options:[
-					[
-					      { text: '全部商品', value: 0 },
-					      { text: '新款商品', value: 1 },
-					      { text: '活动商品', value: 2 },
-					],
-					[
-					      { text: '默认排序', value: 'a' },
-					      { text: '好评排序', value: 'b' },
-					      { text: '销量排序', value: 'c' },
-					],
-					[
-					      { text: '默认排序', value: '3' },
-					      { text: '好评排序', value: '4' },
-					      { text: '销量排序', value: '5' },
-					],
-				]
-			}
-		},
-		onLoad() {
-			// console.log(tabBar)
-		},
-		methods: {
-			// beforeRead(e,callback){
-			// 	console.log(e)
-			// 	callback(true)
-			// },
-			// success(file){
-			// 	console.log(file)
-			// 	file.forEach(item=>{
-			// 		this.fileList.push({url:item.data.url})
-			// 	})
-			// },
-			// fail(err){
-			// 	console.log(err)
-			// },
-			// preView(index){
-			// 	console.log(index)
-			// },
-			// clickDelete(index){
-			// 	console.log(index)
-			// 	this.fileList.splice(index,1)
-			// },
-			// onChange(index){
-			// 	console.log(index)
-			// },
-			// badgeClick(e){
-			// 	console.log(e)
-			// },
-			// leftClick(){
-			// 	console.log('left')
-			// }
-			change(e){
-				console.log(e)
-			}
-		}
-	}
+import uniEcCanvas from "@/components/uni-ec-canvas/uni-ec-canvas";
+export default {
+  data() {
+    return {
+      ec: {
+        option: {
+          title: {
+            text: ""
+          },
+          tooltip: {
+            trigger: "axis",
+            formatter: "{b}\r\n{c0}人",
+            axisPointer: {
+              type: "line",
+              axis: "x",
+              label: {
+                backgroundColor: "#000000"
+              }
+            }
+          },
+          grid: {
+            left: "6%",
+            right: "6%",
+            top: "6%",
+            bottom: "6%",
+            containLabel: true
+          },
+          xAxis: {
+            type: "category",
+            boundaryGap: false,
+            data: ["2-12", "2-14", "2-16", "2-18", "2-20", "2-22", "2-24"],
+            axisLine: {
+              // y轴
+              show: false
+            },
+            axisTick: {
+              // y轴刻度线
+              show: false
+            },
+            splitLine: {
+              // 网格线
+              show: false
+            }
+          },
+          yAxis: {
+            type: "value",
+            axisLine: {
+              // y轴
+              show: false
+            },
+            axisTick: {
+              // y轴刻度线
+              show: false
+            },
+            splitLine: {
+              // 网格线
+              show: false
+            }
+          },
+          series: [
+            {
+              name: "浏览量",
+              type: "line",
+              smooth: true,
+              areaStyle: {
+                color: {
+                  type: "linear",
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: "#E50113" // 0% 处的颜色
+                    },
+                    {
+                      offset: 1,
+                      color: "#fff" // 100% 处的颜色
+                    }
+                  ],
+                  global: false // 缺省为 false
+                }
+              },
+              lineStyle: {
+                color: "#EF5959"
+              },
+              data: [120, 132, 101, 134, 90, 230, 210]
+            }
+          ]
+        }
+      },
+      ec2: {
+        option: {
+          tooltip:{
+            show:true
+          },
+          title: {
+            text: "7,5950",
+            left: "center",
+            top: "40%",
+            subtext: "客户总数",
+            subtextStyle: {
+              color: "#959595",
+              fontSize: 12,
+              align: "center"
+            },
+            textStyle: {
+              color: "#19193E",
+              fontSize: 23,
+              align: "center"
+            }
+          },
+          series: [
+            {
+              name: "访问来源",
+              type: "pie",
+              radius: ["40%", "60%"],
+              label: {
+                position: "outside",
+                color: "#19193E",
+                normal: {
+                  show: false
+                },
+                emphasis: {
+                  show: true
+                },
+                fontSize: 10
+              },
+              labelLine: {
+                normal: {
+                  show: false
+                },
+                emphasis: {
+                  show: true
+                }
+              },
+              data: [
+                {
+                  value: 3321,
+                  name: "直销中心",
+                  itemStyle: {
+                    normal: { color: "#EF5959" },
+                    emphasis: { color: "#EF5959" }
+                  }
+                },
+                {
+                  value: 1148,
+                  name: "手动录入",
+                  itemStyle: {
+                    normal: { color: "#ffa974" },
+                    emphasis: { color: "#ffa974" }
+                  }
+                },
+                {
+                  value: 2532,
+                  name: "线上访客",
+                  itemStyle: {
+                    normal: { color: "#ffc68a" },
+                    emphasis: { color: "#ffc68a" }
+                  }
+                },
+                {
+                  value: 1148,
+                  name: "线下拓客",
+                  itemStyle: { color: "#F0DD93" }
+                },
+                {
+                  value: 1148,
+                  name: "名片海报",
+                  itemStyle: { color: "#E7CB84" }
+                },
+                {
+                  value: 1148,
+                  name: "自然来访",
+                  itemStyle: { color: "#7EBD95" }
+                },
+                { value: 1148, name: "分享家", itemStyle: { color: "#687793" } }
+              ]
+            }
+          ]
+        }
+      }
+    };
+  },
+  onReady() {
+    setTimeout(() => {
+      this.ec.option.series[0].data = [1, 2, 3, 4, 5, 6, 7];
+      console.log("折线图数据改变啦");
+    }, 1000);
+    setTimeout(() => {
+      //数组内的数据要$set哦
+      this.ec2.option.series[0].data[0].value = 0;
+      console.log("折线图数据改变啦");
+    }, 2000);
+  },
+  components: {
+    uniEcCanvas
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.uni-ec-canvas {
+  width: 750upx;
+  height: 750upx;
+  display: block;
+}
 </style>
